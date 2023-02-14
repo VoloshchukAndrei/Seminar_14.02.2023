2fa368e7238f5dff89723a1874c62cab9e6a8e26
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 13:20:25 +0500

    Multiples

---
+++ Multiples/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+int result = numberA % numberB;
+if(result == 0)
+{
+    Console.Write("Числа являются кратными");
+}
+else
+{
+   Console.Write("Числа не являются кратными, остаток " + result); 
+}
