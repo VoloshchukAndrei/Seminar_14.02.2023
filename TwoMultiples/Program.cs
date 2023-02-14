874a77658680f1ae11f4cc90c6417274a65ae1d3
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 14:07:39 +0500

    TowMultiples_a_comment

--- TwoMultiples/Program.cs
+++ TwoMultiples/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 
-if((number % 7 == 0) && (number % 23 == 0))
+if((number % 7 == 0) && (number % 23 == 0)) // && - и
 {
     Console.Write("Число является кратным числам 7 и 23");
 }
