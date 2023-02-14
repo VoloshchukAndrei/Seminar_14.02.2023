54148872c63611a148c0f1e19fb230a540f0794f
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 14:02:20 +0500

    Square tow numbers

---
+++ Square/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if((numberA / numberB == numberB) || (numberB / numberA == numberA))
+{
+    Console.Write("Одно число является квадратом другого");
+}
+else
+{
+   Console.Write("Одно число не является квадратом другого"); 
+}
