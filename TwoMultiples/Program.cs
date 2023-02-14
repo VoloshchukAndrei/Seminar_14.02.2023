b64072271e98264618f8f8f1395ab003654b6c38
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-14 13:37:13 +0500

    TowMultiples

---
+++ TwoMultiples/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if((number % 7 == 0) && (number % 23 == 0))
+{
+    Console.Write("Число является кратным числам 7 и 23");
+}
+else
+{
+   Console.Write("Число не является кратным числам 7 и 23"); 
+}
